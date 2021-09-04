d3bb3aa7201c3096c319088fd46ea913ebc3df12
Shayan-9248 <shayan.aimoradii@gmail.com>
2021-09-04 12:20:12 +0430

    customer registeration form

--- app/customer/routes.py
+++ app/customer/routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from .forms import CustomerRegisterForm
 
 
 blueprint = Blueprint('customers', __name__)
+
+
+@blueprint.route('/customer-register')
+def customer_register():
+    return render_template('customer/register.html')
